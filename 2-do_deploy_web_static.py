b9b6409a4e070db7bade20f9a1c36660c93feb80
agichimu <alexandergichimu98@gmail.com>
2024-02-06 18:20:43 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,47 @@
 #!/usr/bin/python3
-""" Does deployment"""
+"""Fabric script that distributes an archive to your web servers"""
 
 from fabric.api import *
 import os
 
-env.hosts = ["34.73.8.171", "34.74.18.52"]
+env.hosts = ["54.162.37.159", "	18.234.169.238"]
 env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """ Deploys archive to servers"""
+    """Distributes an archive to your web servers"""
     if not os.path.exists(archive_path):
+        print("Error: Archive does not exist")
         return False
 
-    results = []
+    try:
+        archive_name = os.path.basename(archive_path)
+        dest_path = "/tmp/" + archive_name
 
-    res = put(archive_path, "/tmp")
-    results.append(res.succeeded)
+        # Upload archive to /tmp directory on each server
+        put(archive_path, dest_path)
 
-    basename = os.path.basename(archive_path)
-    if basename[-4:] == ".tgz":
-        name = basename[:-4]
-    newdir = "/data/web_static/releases/" + name
-    run("mkdir -p " + newdir)
-    run("tar -xzf /tmp/" + basename + " -C " + newdir)
+        # Uncompress archive to /data/web_static/releases/<archive_name>
+        folder_name = archive_name.replace('.tgz', '').split('/')[-1]
+        remote_path = "/data/web_static/releases/" + folder_name
+        run("mkdir -p " + remote_path)
+        run("tar -xzf " + dest_path + " -C " + remote_path)
 
-    run("rm /tmp/" + basename)
-    run("mv " + newdir + "/web_static/* " + newdir)
-    run("rm -rf " + newdir + "/web_static")
-    run("rm -rf /data/web_static/current")
-    run("ln -s " + newdir + " /data/web_static/current")
+        # Remove archive from /tmp
+        run("rm " + dest_path)
 
-    return True
+        # Move contents from extracted folder to its parent folder
+        run("mv " + remote_path + "/web_static/* " + remote_path)
+
+        # Remove symbolic link if exists
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link
+        run("ln -s " + remote_path + " /data/web_static/current")
+
+        print("New version deployed!")
+        return True
+
+    except Exception as e:
+        print("Error:", str(e))
+        return False
